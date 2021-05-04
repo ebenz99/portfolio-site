0d440fbdcc1d5d33d63f8a533856467d9a4835b0
Ethan Bensman <ebenz99@gmail.com>
2021-05-04 12:53:26 -0400

    start of some sick about css

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,6 @@
 import './About.scss';
 import Blob from'./Blob/Blob';
-import { HOBBIES, INTERESTS, MORE } from '../../constants.js'
+import { HOBBIES } from '../../constants.js'
 
 function About() {
   return (
@@ -8,14 +8,16 @@ function About() {
         <div className="topLine">
             Hi, I'm Ethan. I got my B.S. in Computer Science from Clemson University in 2021, and I'm currently working at AWS.
         </div>
-        <div>
-            <em className="interests">Interests,</em>&nbsp;<em className="hobbies">Hobbies,</em>&nbsp;and&nbsp;<em className="more">more</em>
+        <div className="aboutHeader">
+            <h2 className="aboutHeaderText"><em className="interests">Interests,</em>&nbsp;<em className="hobbies">Hobbies,</em>&nbsp;and&nbsp;<em className="more">more</em></h2>
         </div>
-        {
-            HOBBIES.map((hobby, idx) => {
-                return <Blob type="hobby" id={`hobby${idx}`} key={idx}></Blob>
-            })
-        }
+        <div className="blobFest">
+            {
+                HOBBIES.map((hobby, idx) => {
+                    return <Blob type="hobby" name={hobby} animationName={`hobby${idx}`} key={idx}></Blob>
+                })
+            }
+        </div>
         
     </div>
   );
--- src/components/About/Blob/Blob.js
+++ src/components/About/Blob/Blob.js
@@ -1,9 +1,13 @@
 import './Blob.scss';
+import './keyframes.scss';
 
 function Blob(props) {
+const style = {'animationName': props.animationName, "position": 'relative'}
   return (
-    <div className={`${props.type}Blob blob`}>
-        Hi
+    <div className="ibox">
+      <div style={style} className={`${props.type}Blob blob`}>
+          {props.name}
+      </div>
     </div>
   );
 }
