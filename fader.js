fa3170e846ec04450252ee05abb2ca1fc58941fb
Ethan Bensman <ebenz99@gmail.com>
2021-05-24 22:46:01 -0400

    Updates

--- fader.js
+++ fader.js
@@ -1,15 +1,15 @@
-// function fade(){
-// 	($(".musicHeader")).css("opacity", 1 - (($(document)).scrollTop()) / 100);
-// }
+function fade(){
+	($(".musicHeader")).css("opacity", 1 - (($(document)).scrollTop()) / 100);
+}
 
-// const faderCheck = () => {
-//     if (document.getElementsByClassName("musicHeader").length === 0) {
-//         ;
-//     }
-//     else {
-//         $(window).scroll(fade);
-//     }
-// }
+const faderCheck = () => {
+    if (document.getElementsByClassName("musicHeader").length === 0) {
+        ;
+    }
+    else {
+        $(window).scroll(fade);
+    }
+}
 
 function fade2(){
 	($(".bookHeadingText")).css("opacity", 1 - (($(document)).scrollTop()) / 100);
